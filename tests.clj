34b485b9f79f0591126719306b366d9bb4335f04
Benny Tsai <benny.tsai@gmail.com>
2011-10-22 13:00:55 -0600

    add better tests

--- tests.clj
+++ tests.clj
@@ -1,9 +1,53 @@
 (ns tests
-  (:use [engine]))
+  (:require [core :as core])
+  (:use [clojure.test]))
 
-(let [names ["Charlie" "Bob" "Alice"]
-      cards (shuffle deck)
-      rolls (repeatedly roll-dice)
-      history (race-history (make-state names cards rolls))]
-  (print-history history)
-  (print-end-game (last history)))
+;; Player functions
+(deftest deal-cards
+  (is (= (core/deal-cards (repeat 3 {}) (range 10))
+         [{:cards [0 3 6 9]}
+          {:cards [1 4 7]}
+          {:cards [2 5 8]}])))
+
+(deftest pay-cost
+  (is (= (core/pay-cost {:chips 10} 20)
+         [{:chips 0} 10])))
+
+(deftest pay-card
+  (is (= (core/pay-card [{:chips 50 :cards [1 1]}
+                         {:chips 30 :cards [1 2]}
+                         {:chips 10 :cards [1 3]}]
+                        1
+                        20)
+         [[{:chips 10 :cards [1 1]}
+           {:chips 10 :cards [1 2]}
+           {:chips  0 :cards [1 3]}]
+          70])))
+
+(deftest discard-card
+  (is (= (core/discard-card [{:cards [0 0 0]}
+                             {:cards [0 0 1]}
+                             {:cards [0 1 1]}]
+                            1)
+         [{:cards [0 0 0]}
+          {:cards [0 0]}
+          {:cards [0]}])))
+
+;; Horse functions
+(deftest count-scratched
+  (is (= (core/count-scratched {2 (core.Horse. 1 true)
+                                3 (core.Horse. 2 true)
+                                4 (core.Horse. 0 false)})
+         2)))
+
+(deftest scratch-horse
+  (is (= (core/scratch-horse {2 (core.Horse. 1 true)
+                              3 (core.Horse. 2 true)
+                              4 (core.Horse. 0 false)}
+                             4
+                             3)
+         {2 (core.Horse. 1 true)
+          3 (core.Horse. 2 true)
+          4 (core.Horse. 3 true)})))
+
+(run-tests 'tests)
