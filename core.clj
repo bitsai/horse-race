17c94b3632955cec6bc5af7d756f23c567c46ff8
Benny Tsai <benny.tsai@gmail.com>
2011-10-23 19:59:00 -0600

    update new-scratch, pay-scratch, and advance-horse to call advance-turn

--- core.clj
+++ core.clj
@@ -45,8 +45,9 @@
   (count (filter #(:scratched? %) (vals horses))))
 
 (defn scratch-horse
-  [horses number new-pos]
-  (assoc horses number (Horse. new-pos true)))
+  [horses number]
+  (let [new-pos (inc (count-scratched horses))]
+    (assoc horses number (Horse. new-pos true))))
 
 ;; Game functions
 (defn new-game
@@ -62,32 +63,6 @@
     :pot 0
     :players (deal-cards (:players game) cards)))
 
-(defn new-scratch
-  [game]
-  (let [{:keys [pot players horses rolls]} game
-        new-pos (inc (count-scratched horses))
-        cost (* 5 new-pos)
-        roll (first rolls)
-        [paid-players payment] (pay-card players roll cost)]
-    (assoc game
-      :pot (+ pot payment)
-      :players (discard-card paid-players roll)
-      :horses (scratch-horse horses roll new-pos))))
-
-(defn pay-scratch
-  [game]
-  (let [{:keys [pot players horses rolls]} game
-        cost (* 5 (:position (horses (first rolls))))
-        [paid-player payment] (pay-cost (first players) cost)]
-    (assoc game
-      :pot (+ pot payment)
-      :players (assoc players 0 paid-player))))
-
-(defn advance-horse
-  [game]
-  (let [roll (first (:rolls game))]
-    (update-in game [:horses roll :position] inc)))
-
 (defn advance-player
   [game]
   (let [rotate-vec (fn [v] (conj (subvec v 1) (first v)))]
@@ -97,12 +72,38 @@
   [game]
   (advance-player (update-in game [:rolls] rest)))
 
+(defn new-scratch
+  [game]
+  (let [{:keys [pot players horses rolls]} game
+        new-pos (inc (count-scratched horses))
+        cost (* 5 new-pos)
+        roll (first rolls)
+        [paid-players payment] (pay-card players roll cost)]
+    (advance-turn (assoc game
+                    :pot (+ pot payment)
+                    :players (discard-card paid-players roll)
+                    :horses (scratch-horse horses roll)))))
+
+(defn pay-scratch
+  [game]
+  (let [{:keys [pot players horses rolls]} game
+        cost (* 5 (:position (horses (first rolls))))
+        [paid-player payment] (pay-cost (first players) cost)]
+    (advance-turn (assoc game
+                    :pot (+ pot payment)
+                    :players (assoc players 0 paid-player)))))
+
+(defn advance-horse
+  [game]
+  (let [roll (first (:rolls game))]
+    (advance-turn (update-in game [:horses roll :position] inc))))
+
 (defn play-turn
   [game]
   (let [{:keys [players horses rolls]} game
         player (first players)
         horse (horses (first rolls))]
     (cond (zero? (:chips player))        (advance-player game)
-          (:scratched? horse)            (advance-turn (pay-scratch game))
-          (= 4 (count-scratched horses)) (advance-turn (advance-horse game))
-          :else                          (advance-turn (new-scratch game)))))
+          (:scratched? horse)            (pay-scratch game)
+          (= 4 (count-scratched horses)) (advance-horse game)
+          :else                          (new-scratch game))))
